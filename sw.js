3e347e5e94412a49d729d344e875356ce06e2866
Rimix98 <akademiaroman710@gmail.com>
2026-01-06 21:18:28 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'flickers-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icon.png',
+  '/icon-128.png',
+  '/icon-256.png'
+];
+
+// Установка Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// Активация
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch - сначала сеть, потом кэш
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        // Кэшируем успешные ответы
+        if (response.status === 200) {
+          const responseClone = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => cache.put(event.request, responseClone));
+        }
+        return response;
+      })
+      .catch(() => {
+        // Если сеть недоступна - берём из кэша
+        return caches.match(event.request);
+      })
+  );
+});
